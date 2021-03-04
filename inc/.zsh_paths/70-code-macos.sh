61aad243514fc372df007d6aa524ba2443c2957e
Tarik02 <Taras.Fomin@gmail.com>
2021-03-04 10:01:07 +0200

    fix

--- inc/.zsh_paths/70-code-macos.sh
+++ inc/.zsh_paths/70-code-macos.sh
@@ -1,3 +1,3 @@
-if [ -d /Applications/Visual Studio Code.app/Contents/Resources/app/bin ]; then
+if [ -d "/Applications/Visual Studio Code.app/Contents/Resources/app/bin" ]; then
 	export PATH="/Applications/Visual Studio Code.app/Contents/Resources/app/bin:$PATH"
 fi
--- inc/.zsh_paths/70-subl-macos.sh
+++ inc/.zsh_paths/70-subl-macos.sh
@@ -1,3 +1,3 @@
-if [ -d /Applications/Sublime Text.app/Contents/SharedSupport/bin ]; then
+if [ -d "/Applications/Sublime Text.app/Contents/SharedSupport/bin" ]; then
 	export PATH="/Applications/Sublime Text.app/Contents/SharedSupport/bin:$PATH"
 fi
