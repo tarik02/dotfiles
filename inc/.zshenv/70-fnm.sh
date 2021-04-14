eb682dd4e39db25cc95170c3ef45171bf9840014
Tarik02 <Taras.Fomin@gmail.com>
2021-04-14 11:09:38 +0300

    fix fnm on macos

--- inc/.zshenv/70-fnm.sh
+++ inc/.zshenv/70-fnm.sh
@@ -2,4 +2,6 @@ if [ -f /usr/local/bin/fnm ]; then
 	eval "$(/usr/local/bin/fnm env)"
 elif [ -f $HOME/.fnm/fnm ]; then
 	eval "$($HOME/.fnm/fnm env)"
+elif [ -f /opt/homebrew/bin/fnm ]; then
+	eval "$(/opt/homebrew/bin/fnm env)"
 fi
