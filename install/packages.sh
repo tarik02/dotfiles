daf8286b91a6f601fa2a52155863e02f62d8bfe9
Tarik02 <Taras.Fomin@gmail.com>
2021-08-07 20:07:46 +0300

    support for pacman package manager

--- install/packages.sh
+++ install/packages.sh
@@ -2,9 +2,12 @@
 
 set -e
 
-if [ -f /etc/lsb-release ]; then
+if type apt-get > /dev/null 2>&1; then
 	# Ubuntu
-	$DFI_ROOT/install/packages/ubuntu.sh
+	$DFI_ROOT/install/packages/apt-get.sh
+elif type pacman > /dev/null 2>&1; then
+	# Ubuntu
+	$DFI_ROOT/install/packages/pacman.sh
 fi
 
 $DFI_ROOT/install/packages/common.sh
---
+++ install/packages/pacman.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+sudo pacman -S \
+	git \
+	zsh \
+	unzip \
+	tmux \
+	fasd \
+	wget \
+	curl \
+	vim \
+	htop \
+	gcc \
+	make \
+	ncurses
