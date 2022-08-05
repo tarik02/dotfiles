8a7427819bb7b874f5f5a5f436d6b199f40c7777
Tarik02 <Taras.Fomin@gmail.com>
2022-08-05 12:30:13 +0300

    update keys

--- inc/.zshrc/21-keys.sh
+++ inc/.zshrc/21-keys.sh
@@ -2,8 +2,15 @@ bindkey "^[[1;5C" forward-word
 bindkey "^[[1;5D" backward-word
 bindkey "^[^[[C" forward-word
 bindkey "^[^[[D" backward-word
+
 bindkey "^[[H" beginning-of-line
+bindkey "^[[1~" beginning-of-line
 bindkey "^[[F" end-of-line
+bindkey "^[[4~" end-of-line
+
+bindkey "^[[5~" "beginning-of-buffer-or-history" # pageup
+bindkey "^[[6~" "end-of-buffer-or-history" # pagedown
+bindkey -s "^[[2~" "" # insert
 
 autoload -U up-line-or-beginning-search
 autoload -U down-line-or-beginning-search
