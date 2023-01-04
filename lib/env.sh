19a3fb39c0ab635d2e2551fee67f4c48043e9558
Tarik02 <Taras.Fomin@gmail.com>
2023-01-04 10:51:46 +0200

    fix

--- lib/env.sh
+++ lib/env.sh
@@ -3,8 +3,8 @@ export TERM='xterm-256color'
 
 # XDG
 export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
-export XDG_DATA_HOME="${XDG_DATA_HOME:-$XDG_CONFIG_HOME/local/share}"
-export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$XDG_CONFIG_HOME/cache}"
+#export XDG_DATA_HOME="${XDG_DATA_HOME:-$XDG_CONFIG_HOME/local/share}"
+#export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$XDG_CONFIG_HOME/cache}"
 
 
 # editor
